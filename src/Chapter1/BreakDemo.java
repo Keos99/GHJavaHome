b2a3faa00f70ff43fa81c161b6dde7324e390df3
Keos99 <keos99@bk.ru>
2017-07-31 22:40:01 +0300

    New exercises 3.

---
+++ src/Chapter1/BreakDemo.java
@@ -0,0 +1,20 @@
+package Chapter1;
+
+//Применение оператора break для выхода из цикла
+
+public class BreakDemo {
+    public static void main(String[] args) {
+        int num;
+
+        num = 100;
+
+        // Выполнять цикл до тех пор, пока квадрат значения
+        // переменной i меньше значения переменной num
+
+        for (int i = 0; i < num; i++) {
+            if (i * i >= num) break;
+            System.out.print(i + " ");
+        }
+        System.out.print("Цикл завершен.");
+    }
+}
