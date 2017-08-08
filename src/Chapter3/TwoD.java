fb366be6c9135d19332489ef83eb2a1301af8c18
Keos99 <keos99@bk.ru>
2017-08-09 00:31:52 +0300

    New exercises 6.

---
+++ src/Chapter3/TwoD.java
@@ -0,0 +1,17 @@
+package Chapter3;
+
+public class TwoD {
+    public static void main(String[] args) {
+        int i;
+        int t;
+        int table[][] = new int[3][4];
+
+        for (t = 0; t < 3; t++) {
+            for (i = 0; i < 4; i++) {
+                table[t][i] = (t*4) + i + 1;
+                System.out.print(table[t][i] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
