840cd26c29410ec319a8a9a10fb2df849af523c0
Keos99 <keos99@bk.ru>
2018-05-19 12:43:06 +0300

    Added a homework for the eighth lesson of Java Core: Advenced Level, GeekUniversity.

---
+++ src/GeekUniversityHW/JavaAdvancedlevel/JavaAdvancedlevelHW8/src/server/ServerMain.java
@@ -0,0 +1,8 @@
+package GeekUniversityHW.JavaAdvancedlevel.JavaAdvancedlevelHW8.src.server;
+
+public class ServerMain {
+    public static void main(String[] args) {
+       new Server();
+    }
+
+}
